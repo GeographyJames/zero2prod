7e9bfb0b3e6ba599d4cfab5cbb04d77a513ab691
James Campbell <james.e.campbell@outlook.com>
2024-08-21 15:50:04 +0100

    added health check endpoint and test

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-fn main() {
-    println!("Hello, world!");
-    println!("is the watch working");
-    let a_variable = 10;
+use std::net::TcpListener;
+
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let address = TcpListener::bind("127.0.0.1:8000")?;
+    run(address)?.await
 }
