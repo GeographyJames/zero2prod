3f1d8ad427ff92132b04296c2f35cde2405340a2
James Campbell <james.e.campbell@outlook.com>
2024-08-22 08:31:52 +0100

    added bash docker script for postgres database

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -13,10 +13,53 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let body = "name=james%20campbell&email=james.e.campbell%40outlook.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request");
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = [
+        ("name=james%20campbell", "missing the email"),
+        ("email=james.e.campbell%40outlook.com", "missing the name"),
+        ("", "missing both the email and the name"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request");
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}",
+            error_message
+        )
+    }
+}
+
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("failed to bind to random port.");
     let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind to address");
-    let _ = tokio::spawn(server);
+    let server = zero2prod::startup::run(listener).expect("Failed to bind to address");
+    std::mem::drop(tokio::spawn(server));
     format!("http://127.0.0.1:{}", port)
 }
