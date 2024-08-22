aded1511100d1579882b0f914420c835031d5841
James Campbell <james.e.campbell@outlook.com>
2024-08-22 08:56:41 +0100

    fixed failing test

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,9 +1,10 @@
 use actix_web::{web, HttpResponse};
 
+#[allow(dead_code)]
 #[derive(serde::Deserialize)]
 pub struct FormData {
-    _email: String,
-    _name: String,
+    email: String,
+    name: String,
 }
 
 pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
