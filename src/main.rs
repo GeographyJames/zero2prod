f9f92ec2280a90362311e0fd827d537ce27855bb
James Campbell <james.e.campbell@outlook.com>
2024-08-28 06:31:54 +0100

    Added LogTracer (Finished up to page 117)

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-use std::net::TcpListener;
-
 use sqlx::PgPool;
+use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    init_subscriber(subscriber);
     let configuration = get_configuration().expect("Failed to read configuration file.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
